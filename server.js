9e100e9944d43a34e246424a64a32df04de57c9f
pawankumarfujitsu <pawan.kumar@fujitsu.com>
2020-10-16 12:20:33 +0530

    complete config for node js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const mongoose = require('./routes/mongoose')
 const Users = require('./routes/Users')
 var app= express()
-var PORT = process.env.PORT || 5000
+var PORT = 8008
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -12,13 +12,14 @@ app.use(
     bodyParser.urlencoded({extended: false})
 )
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true});
+// mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true,useCreateIndex: true});
 const connection = mongoose.connection;
 
-connection.once('open', function(){
-    console.log('MongoDB Connected Successfully');
-})
+// connection.once('open', function(){
+//     console.log('MongoDB Connected Successfully');
+// })
 app.use('/users', Users);
+
 
 app.listen(PORT, function(){
     console.log("Server is running on Port :" +PORT);
